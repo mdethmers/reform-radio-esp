fac5f2b5f0eae9e031a2efa1a06fc69d793a4fb8
Matthijs D. <matthijsdethmers@gmail.com>
2025-11-19 13:01:59 +0100

    added button+bt switch

--- REFORM.ino
+++ REFORM.ino
@@ -3,30 +3,121 @@
 #include "AudioTools/Communication/AudioHttp.h"
 #include "AudioTools/AudioLibs/A2DPStream.h"
 
+// Rotary encoder pins
+#define ENCODER_CLK 25
+#define ENCODER_DT 26
+#define ENCODER_SW 27
+
+// CJMCU-1334 DAC uses I2S
+#define I2S_BCLK 14
+#define I2S_LRC 15
+#define I2S_DOUT 22
+
 URLStream url("ssid","password");
-A2DPStream a2dp;  // Bluetooth A2DP output
+A2DPStream a2dp;  // Bluetooth receiver (RX mode)
+I2SStream i2s;    // DAC output
 MP3DecoderHelix codec;
-EncodedAudioStream dec(&a2dp, &codec); // Decoding stream
-StreamCopy copier(dec, url); // copy url to decoder
+
+// Source mode: true = Bluetooth from phone, false = Internet radio
+bool useBluetoothInput = false;
+
+EncodedAudioStream dec(&i2s, &codec); // For internet radio decoding
+StreamCopy copier_radio(dec, url);    // Internet radio -> decoder -> DAC
+StreamCopy copier_bt(i2s, a2dp);      // Bluetooth -> DAC
+
+// Volume control
+int volume = 50; // 0-100
+int lastCLK;
+unsigned long lastButtonPress = 0;
+
+void IRAM_ATTR encoderISR() {
+  int clkState = digitalRead(ENCODER_CLK);
+  int dtState = digitalRead(ENCODER_DT);
+  
+  if (clkState != lastCLK && clkState == LOW) {
+    if (dtState != clkState) {
+      volume = min(100, volume + 5);
+    } else {
+      volume = max(0, volume - 5);
+    }
+    Serial.print("Volume: ");
+    Serial.println(volume);
+    
+    // Apply volume to DAC
+    i2s.setVolume(volume / 100.0);
+  }
+  lastCLK = clkState;
+}
+
+void IRAM_ATTR buttonISR() {
+  unsigned long currentTime = millis();
+  if (currentTime - lastButtonPress > 300) { // Debounce
+    useBluetoothInput = !useBluetoothInput;
+    lastButtonPress = currentTime;
+    
+    Serial.print("Switched to: ");
+    Serial.println(useBluetoothInput ? "Bluetooth Input" : "Internet Radio");
+    
+    // Switch input source
+    if (useBluetoothInput) {
+      // Stop internet radio
+      url.end();
+      dec.end();
+      
+      // Start Bluetooth receiver
+      auto a2dp_cfg = a2dp.defaultConfig(RX_MODE);
+      a2dp_cfg.name = "Arduino Speaker";
+      a2dp.begin(a2dp_cfg);
+    } else {
+      // Stop Bluetooth
+      a2dp.end();
+      
+      // Start internet radio
+      dec.begin();
+      url.begin("http://stream.srg-ssr.ch/m/rsj/mp3_128","audio/mp3");
+    }
+  }
+}
 
 void setup(){
   Serial.begin(115200);
   AudioToolsLogger.begin(Serial, AudioToolsLogLevel::Info);
 
-  // setup Bluetooth A2DP
-  auto cfg = a2dp.defaultConfig(TX_MODE);
-  cfg.name = "Arduino Audio";
-  a2dp.begin(cfg);
+  // Setup rotary encoder
+  pinMode(ENCODER_CLK, INPUT_PULLUP);
+  pinMode(ENCODER_DT, INPUT_PULLUP);
+  pinMode(ENCODER_SW, INPUT_PULLUP);
+  lastCLK = digitalRead(ENCODER_CLK);
+  
+  attachInterrupt(digitalPinToInterrupt(ENCODER_CLK), encoderISR, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_SW), buttonISR, FALLING);
 
-  // setup decoder
+  // Setup I2S for CJMCU-1334 DAC (always active)
+  auto i2s_cfg = i2s.defaultConfig(TX_MODE);
+  i2s_cfg.pin_bck = I2S_BCLK;
+  i2s_cfg.pin_ws = I2S_LRC;
+  i2s_cfg.pin_data = I2S_DOUT;
+  i2s_cfg.sample_rate = 44100;
+  i2s_cfg.bits_per_sample = 16;
+  i2s_cfg.channels = 2;
+  i2s.begin(i2s_cfg);
+  i2s.setVolume(volume / 100.0);
+
+  // Start with Internet Radio mode
   dec.begin();
-
-  // mp3 radio
   url.begin("http://stream.srg-ssr.ch/m/rsj/mp3_128","audio/mp3");
 
-  Serial.println("Bluetooth 'Arduino Audio' ready - pair with your phone");
+  Serial.println("Ready - Internet Radio mode active");
+  Serial.println("Rotate encoder: volume control");
+  Serial.println("Click encoder: switch Bluetooth/Radio");
 }
 
 void loop(){
-  copier.copy();
+  if (useBluetoothInput) {
+    // Stream Bluetooth audio from phone to DAC
+    copier_bt.copy();
+  } else {
+    // Stream internet radio to DAC
+    copier_radio.copy();
+  }
 }
